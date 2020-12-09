bad7c114385e71a4e170fee4deee0282c339a149
ari1021 <o.pe.3.vndt.ku@gmail.com>
2020-12-09 20:08:53 +0900

    excludesallを消去

--- model/user.go
+++ model/user.go
@@ -2,7 +2,7 @@ package model
 
 type User struct {
 	ID   string
-	Name string `form:"name" validate:"required, excludesall= "`
+	Name string `form:"name" validate:"required"`
 }
 
 var Users = map[string]*User{}
