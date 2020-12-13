10d0a3001642aced18e012cde3de0b8be9890975
ari1021 <45930091+ari1021@users.noreply.github.com>
2020-12-14 01:16:35 +0900

    Update model/user.go
    Co-authored-by: Shuntaro Shimada <29503528+konnyaku256@users.noreply.github.com>

--- model/user.go
+++ model/user.go
@@ -2,7 +2,7 @@ package model
 
 type User struct {
 	ID   string
-	Name string `form:"name" validate:"required"`
+      Name string `form:"name" validate:"required,excludesall= "`
 }
 
 var Users = map[string]*User{}
