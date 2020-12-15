383fe7c8d6cabb20d5a8d7138497bb3f53a1a29c
ari1021 <o.pe.3.vndt.ku@gmail.com>
2020-12-15 13:58:56 +0900

    validationに関する部分をapi/validation.goに移行

--- server/api/router.go
+++ server/api/router.go
@@ -8,14 +8,6 @@ import (
 	"gopkg.in/go-playground/validator.v9"
 )
 
-type customValidator struct {
-	Validator *validator.Validate
-}
-
-func (cv *customValidator) Validate(i interface{}) error {
-	return cv.Validator.Struct(i)
-}
-
 func NewEcho(hub *websocket.Hub) *echo.Echo {
 	e := echo.New()
 	e.Use(middleware.Logger())
