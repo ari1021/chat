874d2d876c5bde175610abc4e755cf6b64895c69
ari1021 <o.pe.3.vndt.ku@gmail.com>
2021-01-14 01:02:45 +0900

    fix: golangci_lintが通るようにまだ使ってないtokenをコメントアウト

--- model/user.go
+++ model/user.go
@@ -7,14 +7,14 @@ import (
 )
 
 type User struct {
-	ID           string `gorm:"primaryKey"`
-	CreatedAt    time.Time
-	UpdatedAt    time.Time
-	DeletedAt    gorm.DeletedAt `gorm:"index"`
-	Name         string         `form:"name" validate:"required,excludesall= " gorm:"unique;not null"`
-	idToken      string
-	accessToken  string
-	refreshToken string
+	ID        string `gorm:"primaryKey"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+	Name      string         `form:"name" validate:"required,excludesall= " gorm:"unique;not null"`
+	// idToken      string
+	// accessToken  string
+	// refreshToken string
 }
 
 var Users = map[string]*User{}
