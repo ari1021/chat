a5ba3b55d2e8e2460f041fd1eb7df9a94209d039
ari1021 <o.pe.3.vndt.ku@gmail.com>
2021-01-20 02:27:41 +0900

    fix: request/DeleteRoomを削除

--- server/request/room.go
+++ server/request/room.go
@@ -4,7 +4,3 @@ type CreateRoom struct {
 	Name   string `form:"name" validate:"required,excludesall= "`
 	UserID string `form:"user_id" validate:"required"`
 }
-
-type DeleteRoom struct {
-	ID uint `form:"id" validate:"required"`
-}
