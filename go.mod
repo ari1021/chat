2976099a1eaa05552f51cd5ff346540bb8236a58
ari1021 <o.pe.3.vndt.ku@gmail.com>
2021-01-23 14:55:09 +0900

    feat: IsDuplicateKeyErrorとIsForeignKeyErrorを追加

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ replace gopkg.in/urfave/cli.v2 => github.com/urfave/cli/v2 v2.2.0 // Need to use
 
 require (
 	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/go-sql-driver/mysql v1.5.0
 	github.com/google/uuid v1.1.2
 	github.com/gorilla/websocket v1.4.2
 	// github.com/labstack/echo v3.3.10+incompatible
--- model/error.go
+++ model/error.go
@@ -1,6 +1,32 @@
 package model
 
+import (
+	"github.com/go-sql-driver/mysql"
+)
+
 type APIError struct {
 	StatusCode int
 	Message    string
+}
+
+func IsDuplicateKeyError(err error) bool {
+	me, ok := err.(*mysql.MySQLError)
+	if !ok {
+		return false
+	}
+	if me.Number == 1062 {
+		return true
+	}
+	return false
+}
+
+func IsForeignKeyError(err error) bool {
+	me, ok := err.(*mysql.MySQLError)
+	if !ok {
+		return false
+	}
+	if me.Number == 1452 {
+		return true
+	}
+	return false
 }
