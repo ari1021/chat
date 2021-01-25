f8fd96e0425e9e04f0e3bf265656d0d9ed5274c1
ari1021 <o.pe.3.vndt.ku@gmail.com>
2021-01-25 20:27:29 +0900

    feat: controllerパッケージにCreateChatを作成

---
+++ controller/chat.go
@@ -0,0 +1,44 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/ari1021/websocket/db"
+	"github.com/ari1021/websocket/model"
+	"github.com/ari1021/websocket/server/request"
+	"github.com/labstack/echo/v4"
+)
+
+func CreateChat(c echo.Context) error {
+	req := &request.CreateChat{}
+	if err := c.Bind(req); err != nil {
+		return err
+	}
+	if err := c.Validate(req); err != nil {
+		res := model.NewAPIError(400, "chat unprocessable entity")
+		return c.JSON(http.StatusBadRequest, res)
+	}
+	conn := db.DB.GetConnection()
+	chat := &model.Chat{
+		RoomID:  req.RoomID,
+		UserID:  req.UserID,
+		Message: req.Message,
+	}
+	if _, err := chat.Create(conn); err != nil {
+		switch model.CheckMySQLError(err) {
+		case model.ForeignKeyError:
+			res := model.NewAPIError(400, "foreign key error")
+			return c.JSON(http.StatusBadRequest, res)
+		case model.DuplicateKeyError:
+			res := model.NewAPIError(400, "duplicate key error")
+			return c.JSON(http.StatusBadRequest, res)
+		case model.DatabaseError:
+			res := model.NewAPIError(500, "database error")
+			return c.JSON(http.StatusInternalServerError, res)
+		default:
+			res := model.NewAPIError(500, "unknown error")
+			return c.JSON(http.StatusInternalServerError, res)
+		}
+	}
+	return c.JSON(http.StatusOK, chat)
+}
