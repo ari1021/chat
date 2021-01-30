fbbc9762ad6d4bde56a2130a0317924776df1cf0
ari1021 <o.pe.3.vndt.ku@gmail.com>
2021-01-30 15:00:26 +0900

    fix: request.CreateChatにUserNameを追加

--- server/request/chat.go
+++ server/request/chat.go
@@ -3,10 +3,11 @@ package request
 type GetChats struct {
 	RoomID int `param:"id"`
 	Limit  int `query:"limit" validate:"required"`
-	Offset int `query:"offset" validate:"required"`
+	Offset int `query:"offset"`
 }
 
 type CreateChat struct {
-	Message string `form:"message" validate:"required,excludesall= "`
-	RoomID  int    `param:"id"`
+	Message  string `form:"message" validate:"required,excludesall= "`
+	RoomID   int    `param:"id"`
+	UserName string `form:"user_name" validate:"required,excludesall= "`
 }
