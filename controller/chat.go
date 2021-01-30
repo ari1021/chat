244719acf460c13ebffde4ad676a84ea235c94f8
ari1021 <o.pe.3.vndt.ku@gmail.com>
2021-01-30 15:00:01 +0900

    fix: フロントからUserNameを取得するように修正

--- controller/chat.go
+++ controller/chat.go
@@ -20,8 +20,9 @@ func CreateChat(c echo.Context) error {
 	}
 	conn := db.DB.GetConnection()
 	chat := &model.Chat{
-		RoomID:  req.RoomID,
-		Message: req.Message,
+		RoomID:   req.RoomID,
+		Message:  req.Message,
+		UserName: req.UserName,
 	}
 	if _, err := chat.Create(conn); err != nil {
 		statusCode, res := model.NewAPIResponse(err)
