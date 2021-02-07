74b22fb88aa4b2545ff2c8ebf2e73dca703105e2
ari1021 <o.pe.3.vndt.ku@gmail.com>
2021-02-07 16:36:28 +0900

    refactor: createChat, GetChatsをChatHandlerのmethodとして再定義

--- controller/chat.go
+++ controller/chat.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 
 	"github.com/ari1021/websocket/model"
-	"github.com/ari1021/websocket/server/db"
 	"github.com/ari1021/websocket/server/request"
 	"github.com/labstack/echo/v4"
 )
@@ -19,7 +18,7 @@ func NewChatHandler(IChat model.IChat) ChatHandler {
 	}
 }
 
-func CreateChat(c echo.Context) error {
+func (ch ChatHandler) CreateChat(c echo.Context) error {
 	req := &request.CreateChat{}
 	if err := c.Bind(req); err != nil {
 		return err
@@ -28,20 +27,16 @@ func CreateChat(c echo.Context) error {
 		res := model.NewAPIError(400, "chat unprocessable entity")
 		return c.JSON(http.StatusBadRequest, res)
 	}
-	conn := db.DB.GetConnection()
-	chat := &model.Chat{
-		RoomID:   req.RoomID,
-		Message:  req.Message,
-		UserName: req.UserName,
-	}
-	if _, err := chat.Create(conn); err != nil {
+	ic := ch.IChat
+	chat, err := ic.Create(req.Message, req.RoomID, req.UserName)
+	if err != nil {
 		statusCode, res := model.NewAPIResponse(err)
 		return c.JSON(statusCode, res)
 	}
 	return c.JSON(http.StatusOK, chat)
 }
 
-func GetChats(c echo.Context) error {
+func (ch ChatHandler) GetChats(c echo.Context) error {
 	req := &request.GetChats{}
 	if err := c.Bind(req); err != nil {
 		return err
@@ -50,9 +45,8 @@ func GetChats(c echo.Context) error {
 		res := model.NewAPIError(400, "limit or offset not found")
 		return c.JSON(http.StatusBadRequest, res)
 	}
-	conn := db.DB.GetConnection()
-	chats := &model.Chats{}
-	chats, err := chats.Find(conn, req.RoomID, req.Limit, req.Offset)
+	ic := ch.IChat
+	chats, err := ic.Find(req.RoomID, req.Limit, req.Offset)
 	if err != nil {
 		res := model.NewAPIError(500, "database error")
 		return c.JSON(http.StatusInternalServerError, res)
